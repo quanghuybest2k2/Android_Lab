21b1fd3912a65dbad1ef2f882b88d9f60bfb1c29
Doan Quang Huy <quanghuybest@gmail.com>
2022-11-05 18:44:52 +0700

    update xu ly login ui

--- Lab4/Login_UI/app/src/main/java/vn/dlu/login/MainActivity.java
+++ Lab4/Login_UI/app/src/main/java/vn/dlu/login/MainActivity.java
@@ -4,37 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText edtEmail, edtPass;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.m001_act_login);
-        initControl();
+        initUI();
     }
 
-    public void initControl() {
-        EditText EtxtEmail = findViewById(R.id.txtEmail);
-        EditText EtxtPassWord = findViewById(R.id.txtPassword);
-        TextView txtLogin = findViewById(R.id.txtLogin);
-        txtLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (EtxtEmail.getText().length() > 0 && EtxtPassWord.getText().length() > 0) {
-                    String toastMessage = "Email: " + EtxtEmail.getText().toString() + ", Password: " + EtxtPassWord.getText().toString();
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-                } else {
-                    String toastMessage = "Username or Password are not populated";
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+    private void initUI() {
+        edtEmail = (EditText) findViewById(R.id.txtEmail);
+        edtPass = (EditText) findViewById(R.id.txtPassword);
+        login = (TextView) findViewById(R.id.txtLogin);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.txtLogin:
+                if (!edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty()) {
+                    showToast(edtEmail.getText().toString(), edtPass.getText().toString());
+
                 }
-            }
-        });
+                break;
+        }
+    }
+
+    private void showToast(String email, String pass) {
+        Toast toast = new Toast(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.layout_custom_toast));
+        TextView txtMess = view.findViewById(R.id.txtView);
+        txtMess.setText("Email là: " + email + "\nMật khẩu là: " + pass);
+        toast.setView(view);
+
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.show();
     }
 }
